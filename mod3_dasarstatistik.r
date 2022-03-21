bacb0058191613f1df2a2d70446c0ee1a54c4613
mfachrulrazy <mfachrulrazy@gmail.com>
2022-03-21 10:53:25 +0800

    modul 3 statistics using R

--- mod3_dasarstatistik.r
+++ mod3_dasarstatistik.r
@@ -85,3 +85,41 @@ sd(data_intro$Pendapatan)
 
 ## carilah summary data dari data_intro
 summary(data_intro)
+
+
+# Task 2
+## Carilah sebaran data kolom Jenis.Kelamin dari variable data_intro
+plot(data_intro$Jenis.Kelamin)
+
+## Carilah sebaran data dari Pendapatan dari variable data_intro
+hist(data_intro$Pendapatan)
+
+## Carilah sebaran data dari Produk dari variable data_intro
+plot(data_intro$Produk)
+
+## Carilah sebaran data dari Harga dari variable data_intro
+hist(data_intro$Harga)
+
+## Carilah sebaran data dari Jumlah dari variable data_intro
+hist(data_intro$Jumlah)
+
+## Carilah sebaran data dari Total dari variable data_intro
+hist(data_intro$Total)
+
+## Carilah sebaran data dari Tingkat.Kepuasan dari variable data_intro
+plot(data_intro$Tingkat.Kepuasan)
+
+## Task 3
+# membuat scatter plot untuk melihat arah dan kekuatan korelasi
+plot(data_intro$Pendapatan,data_intro$Total)
+
+## Task 4
+#Gunakan cor.test untuk mencari hubungan Pendapatan dengan Total Belanja 
+cor.test(data_intro$Pendapatan,data_intro$Total)
+
+## Task 5
+## carilah boxplot antara variabel jenis kelamin dengan total belanja
+boxplot(Total~Jenis.Kelamin,data = data_intro)
+
+## analisis bagaimana hubungan jenis kelamin dengan total belanja mengunakan uji statistik t-test
+t.test(Total~Jenis.Kelamin,data = data_intro)
